c72355176b02d6b0934aac354c4ef964f960b684
Aldan Maulana F <mafa.alfa75@gmail.com>
2022-12-08 17:23:43 +0700

    Add files via upload

---
+++ database/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+define("HOST", "localhost");
+define("USER", "root");
+define("PASS", "");
+define("DB", "");
+
+define("DOMAIN","http://localhost/phpSandika/inventory/public_html");
+
+?>
